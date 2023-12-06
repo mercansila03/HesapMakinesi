7b6493d4f2d119620179b7307206186b396646a4
SılaMercan <120477952+mercansila03@users.noreply.github.com>
2023-12-06 23:47:00 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace HesapMakinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.btnCikar = new System.Windows.Forms.Button();
+            this.btnCarp = new System.Windows.Forms.Button();
+            this.btnBol = new System.Windows.Forms.Button();
+            this.btnModAl = new System.Windows.Forms.Button();
+            this.btnCikis = new System.Windows.Forms.Button();
+            this.sayi1 = new System.Windows.Forms.TextBox();
+            this.sayi2 = new System.Windows.Forms.TextBox();
+            this.sonuc = new System.Windows.Forms.Label();
+            this.sonuc2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(194)))), ((int)(((byte)(197)))));
+            this.btnTopla.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTopla.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnTopla.Location = new System.Drawing.Point(51, 29);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(137, 52);
+            this.btnTopla.TabIndex = 0;
+            this.btnTopla.Text = "TOPLA";
+            this.btnTopla.UseVisualStyleBackColor = false;
+            this.btnTopla.Click += new System.EventHandler(this.btnTopla_Click);
+            // 
+            // btnCikar
+            // 
+            this.btnCikar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(194)))), ((int)(((byte)(197)))));
+            this.btnCikar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCikar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnCikar.Location = new System.Drawing.Point(51, 100);
+            this.btnCikar.Name = "btnCikar";
+            this.btnCikar.Size = new System.Drawing.Size(137, 52);
+            this.btnCikar.TabIndex = 1;
+            this.btnCikar.Text = "ÇIKAR";
+            this.btnCikar.UseVisualStyleBackColor = false;
+            this.btnCikar.Click += new System.EventHandler(this.btnCikar_Click);
+            // 
+            // btnCarp
+            // 
+            this.btnCarp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(194)))), ((int)(((byte)(197)))));
+            this.btnCarp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCarp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnCarp.Location = new System.Drawing.Point(51, 171);
+            this.btnCarp.Name = "btnCarp";
+            this.btnCarp.Size = new System.Drawing.Size(137, 52);
+            this.btnCarp.TabIndex = 2;
+            this.btnCarp.Text = "ÇARP";
+            this.btnCarp.UseVisualStyleBackColor = false;
+            this.btnCarp.Click += new System.EventHandler(this.btnCarp_Click);
+            // 
+            // btnBol
+            // 
+            this.btnBol.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(194)))), ((int)(((byte)(197)))));
+            this.btnBol.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBol.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnBol.Location = new System.Drawing.Point(51, 242);
+            this.btnBol.Name = "btnBol";
+            this.btnBol.Size = new System.Drawing.Size(137, 52);
+            this.btnBol.TabIndex = 3;
+            this.btnBol.Text = "BÖL";
+            this.btnBol.UseVisualStyleBackColor = false;
+            this.btnBol.Click += new System.EventHandler(this.btnBol_Click);
+            // 
+            // btnModAl
+            // 
+            this.btnModAl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(194)))), ((int)(((byte)(197)))));
+            this.btnModAl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModAl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnModAl.Location = new System.Drawing.Point(51, 313);
+            this.btnModAl.Name = "btnModAl";
+            this.btnModAl.Size = new System.Drawing.Size(137, 52);
+            this.btnModAl.TabIndex = 4;
+            this.btnModAl.Text = "MOD AL";
+            this.btnModAl.UseVisualStyleBackColor = false;
+            this.btnModAl.Click += new System.EventHandler(this.btnModAl_Click);
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(176)))), ((int)(((byte)(206)))));
+            this.btnCikis.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCikis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCikis.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(88)))), ((int)(((byte)(120)))));
+            this.btnCikis.Location = new System.Drawing.Point(600, 29);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(90, 336);
+            this.btnCikis.TabIndex = 5;
+            this.btnCikis.Text = "ÇIKIŞ";
+            this.btnCikis.UseVisualStyleBackColor = false;
+            this.btnCikis.Click += new System.EventHandler(this.btnCikis_Click);
+            // 
+            // sayi1
+            // 
+            this.sayi1.Location = new System.Drawing.Point(336, 114);
+            this.sayi1.Name = "sayi1";
+            this.sayi1.Size = new System.Drawing.Size(188, 22);
+            this.sayi1.TabIndex = 8;
+            // 
+            // sayi2
+            // 
+            this.sayi2.Location = new System.Drawing.Point(336, 175);
+            this.sayi2.Name = "sayi2";
+            this.sayi2.Size = new System.Drawing.Size(188, 22);
+            this.sayi2.TabIndex = 9;
+            // 
+            // sonuc
+            // 
+            this.sonuc.AutoSize = true;
+            this.sonuc.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sonuc.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.sonuc.Location = new System.Drawing.Point(297, 278);
+            this.sonuc.Name = "sonuc";
+            this.sonuc.Size = new System.Drawing.Size(83, 25);
+            this.sonuc.TabIndex = 10;
+            this.sonuc.Text = "Sonuç=";
+            // 
+            // sonuc2
+            // 
+            this.sonuc2.AutoSize = true;
+            this.sonuc2.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sonuc2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.sonuc2.Location = new System.Drawing.Point(386, 278);
+            this.sonuc2.Name = "sonuc2";
+            this.sonuc2.Size = new System.Drawing.Size(62, 25);
+            this.sonuc2.TabIndex = 11;
+            this.sonuc2.Text = "label";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(171)))), ((int)(((byte)(162)))));
+            this.label1.Location = new System.Drawing.Point(0, 478);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "(c)Sıla MERCAN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(297, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 25);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "a=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label3.Location = new System.Drawing.Point(297, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "b=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(225)))), ((int)(((byte)(221)))));
+            this.ClientSize = new System.Drawing.Size(737, 494);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sonuc2);
+            this.Controls.Add(this.sonuc);
+            this.Controls.Add(this.sayi2);
+            this.Controls.Add(this.sayi1);
+            this.Controls.Add(this.btnCikis);
+            this.Controls.Add(this.btnModAl);
+            this.Controls.Add(this.btnBol);
+            this.Controls.Add(this.btnCarp);
+            this.Controls.Add(this.btnCikar);
+            this.Controls.Add(this.btnTopla);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hesap Makinesi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.Button btnCikar;
+        private System.Windows.Forms.Button btnCarp;
+        private System.Windows.Forms.Button btnBol;
+        private System.Windows.Forms.Button btnModAl;
+        private System.Windows.Forms.Button btnCikis;
+        private System.Windows.Forms.TextBox sayi1;
+        private System.Windows.Forms.TextBox sayi2;
+        private System.Windows.Forms.Label sonuc;
+        private System.Windows.Forms.Label sonuc2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+            double Toplama= Convert.ToDouble(sayi1.Text) + Convert.ToDouble(sayi2.Text);
+            sonuc2.Text = Convert.ToString(Toplama);
+        }
+
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();        
+        }
+
+        private void btnCikar_Click(object sender, EventArgs e)
+        {
+            double Cikarma= Convert.ToDouble(sayi1.Text) - Convert.ToDouble(sayi2.Text);
+            sonuc2.Text = Convert.ToString(Cikarma);
+        }
+
+        private void btnCarp_Click(object sender, EventArgs e)
+        {
+            double Carpma = Convert.ToDouble(sayi1.Text) * Convert.ToDouble(sayi2.Text);
+            sonuc2.Text = Convert.ToString(Carpma);
+        }
+
+        private void btnBol_Click(object sender, EventArgs e)
+        {
+            double Bolme = Convert.ToDouble(sayi1.Text) / Convert.ToDouble(sayi2.Text);
+            sonuc2.Text = Convert.ToString(Bolme);
+        }
+
+        private void btnModAl_Click(object sender, EventArgs e)
+        {
+            double ModAlma= Convert.ToDouble(sayi1.Text) % Convert.ToDouble(sayi2.Text);
+            sonuc2.Text = Convert.ToString(ModAlma);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
